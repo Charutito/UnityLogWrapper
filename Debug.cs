653c6105c3db4d7b711ba338e17020cbae9559db
Mafiu <mf.milewski@gmail.com>
2021-03-22 17:37:45 -0300

    Add project files.

---
+++ Debug.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Methods of this class are called only if "LOGS_ENABLED" define is added to Scripting Define Symbols in Player Settings on Unity.
+/// </summary>
+public sealed class Debug
+{
+    private const string LOGS_ENABLED = "LOGS_ENABLED";
+    private const string UNITY_ASSERTIONS = "UNITY_ASSERTIONS";
+    private const string UNITY_EDITOR = "UNITY_EDITOR";
+}
